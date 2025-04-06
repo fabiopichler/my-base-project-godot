9d30fe8adeef0462fccbb9489a1daa92f93ab0d4
Fábio Pichler <fabiopichler1992@gmail.com>
2025-04-06 16:35:36 -0300

    updated

--- components/teleport.gd
+++ components/teleport.gd
@@ -1,9 +1,29 @@
 extends MeshInstance3D
 
 
-@export_group("Properties")
+enum Fade {
+	Normal,
+	White,
+	None
+}
+
+
 @export var to_map: String = ""
 
 
+@export_group("Fade")
+@export var fade: Fade = Fade.Normal
+@export var fade_duration: float = 0.25
+
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	if to_map.is_empty():
+		return
+	
+	if fade == Fade.Normal:
+		await Scene.fade_out(fade_duration)
+	
 	GameMap.load_map(to_map)
+	
+	if fade == Fade.Normal:
+		await Scene.fade_in(fade_duration)
